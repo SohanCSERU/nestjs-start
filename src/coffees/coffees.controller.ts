0322bdbbb338214a0b8dbbd8e4ee1b594f7c006c
sohancseru <israkcse@gmail.com>
2023-05-15 17:09:25 +0600

    Added some work on it

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  @Get()
+  findAll() {
+    // const { limit, offset } = paginationQuery;
+    // return `This action returns all coffees Limit: ${limit}, offset: ${offset} !`;
+    return this.coffeesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    // console.log('id: ' + id);
+    // return `This action return ${id} coffees`;
+    return this.coffeesService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesService.create(createCoffeeDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    // console.log(body);
+    // return `This action update ${id} coffees`;
+    return this.coffeesService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    // return `This action delete ${id} coffee`;
+    return this.coffeesService.remove(id);
+  }
+}
---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,43 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Chilled Coffee',
+      brand: 'ispahani mirzapur',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    // throw 'A random error';
+    const coffee = this.coffees.find((item) => item.id === +id);
+    if (!coffee) {
+      throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+    }
+    return coffee;
+  }
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCOffee = this.findOne(id);
+    if (existingCOffee) {
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
