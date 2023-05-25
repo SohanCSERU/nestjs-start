7f9c1b135f4f085fbd45c5e8aec09295d3a39f4b
sohancseru <israkcse@gmail.com>
2023-05-25 17:49:39 +0600

    Extra all files are added

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World whats!';
-  }
+	getHello(): string {
+		return "Hello World What's Up!";
+	}
 }
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,19 +1,65 @@
-import { Module } from '@nestjs/common';
+import { Module, Injectable } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Coffee, CoffeeSchema } from './entities/coffee.entity';
+import { EventSchema } from 'src/events/entities/event.entity/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
+import { Connection } from 'mongoose';
 
+// class ConfigService {}
+// class DevelopmentConfigService {}
+// class ProductionConfigService {}
+// class MockCoffeesService {}
+
+// @Injectable()
+// export class CoffeeBrandsFactory {
+//   create() {
+//     return ['buddy brew', 'nescafe'];
+//   }
+// }
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      {
-        name: Coffee.name,
-        schema: CoffeeSchema,
-      },
-    ]),
-  ],
-  controllers: [CoffeesController],
-  providers: [CoffeesService],
+	imports: [
+		MongooseModule.forFeature([
+			{
+				name: Coffee.name,
+				schema: CoffeeSchema,
+			},
+			{
+				name: Event.name,
+				schema: EventSchema,
+			},
+		]),
+	],
+	controllers: [CoffeesController],
+	providers: [
+		CoffeesService,
+		{
+			provide: COFFEE_BRANDS,
+			useFactory: async (connection: Connection): Promise<string[]> => {
+				const coffeeBrand = await Promise.resolve([
+					'buddy brew',
+					'nescafe',
+				]);
+				console.log('[!] async Factory!');
+				return coffeeBrand;
+			},
+		},
+		// {
+		//   provide: ConfigService,
+		//   useClass:
+		//     process.env.NODE_ENV === 'development'
+		//        DevelopmentConfigService
+		//       : ProductionConfigService,
+		// },
+		// CoffeeBrandsFactory,
+		// {
+		//   provide: COFFEE_BRANDS,
+		//   useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+		//     brandsFactory.create(),
+		//   inject: [CoffeeBrandsFactory],
+		// },
+	],
+	exports: [CoffeesService],
 })
 export class CoffeesModule {}
